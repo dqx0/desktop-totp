1e08579e96e719209f639855611b2dcd74ee44dd
dqx0 <38395087+dqx0@users.noreply.github.com>
2024-09-29 03:53:35 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Desktop TOTP Application
+
+このアプリケーションは、TOTP（Time-based One-Time Password）コードを生成し、システムトレイに表示するGo言語で書かれたプログラムです。TOTPコードをクリップボードにコピーすることもできます。
+
+## 特徴
+- システムトレイにTOTPコードを表示
+- アイコンクリックでTOTPコードをクリップボードにコピー
+- PC起動時に自動的に実行（任意設定）
+
+## 必要条件
+- Go 1.23以降
+
+## インストール
+
+1. リポジトリをクローンします。
+
+    ```sh
+    git clone https://github.com/dqx0/desktop-totp.git
+    cd desktop-totp
+    ```
+
+2. 依存関係をインストールします。
+
+    ```sh
+    go mod tidy
+    ```
+
+3. （任意）プログラムをビルドします。
+
+    ```sh
+    go build -ldflags "-H=windowsgui" -o C:\path\to\your\myapp.exe main.go
+    ```
+
+4. （任意）PC起動時に自動実行するために、スタートアップフォルダにショートカットを作成します。以下のPowerShellスクリプトを実行してください（Windowsの場合）。
+
+    ```powershell
+    $sourcePath = "C:\path\to\your\myapp.exe"
+    $shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\myapp.lnk"
+
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $shortcut = $WScriptShell.CreateShortcut($shortcutPath)
+    $shortcut.TargetPath = $sourcePath
+    $shortcut.Save()
+    ```
+
+## 環境変数の設定
+
+以下の環境変数を設定してください。
+
+- `TOTP_SECRET`: TOTPのシークレットキー
+- `TOTP_ICON_PATH`: アイコンファイル（.ico）のパス
+
+## 使用方法
+
+### ビルドした実行ファイルを使用する場合
+
+1. プログラムを実行します。
+
+    ```sh
+    C:\path\to\your\myapp.exe
+    ```
+
+2. システムトレイにアイコンが表示されます。
+3. 「Copy」をクリックすると、TOTPコードがクリップボードにコピーされます。
+
+### `go run`コマンドを使用する場合
+
+1. プログラムを実行します。
+
+    ```sh
+    go run main.go
+    ```
+
+2. システムトレイにアイコンが表示されます。
+3. 「Copy」をクリックすると、TOTPコードがクリップボードにコピーされます。
+
+### 自動実行の設定をした場合
+
+1. 初回は再起動が必要です。
+2. システムトレイにアイコンが表示されます。
+3. 「Copy」をクリックすると、TOTPコードがクリップボードにコピーされます。
+
+## ライセンス
+
+このプロジェクトはApache 2.0ライセンスの下で公開されています。詳細については、LICENSEファイルを参照してください。
+
+このプロジェクトは以下のライブラリを使用しています：
+
+- `atotto/clipboard` (BSD 3-Clause License)
+- `getlantern/systray` (Apache 2.0 License)
+- `pquerna/otp` (Apache 2.0 License)
---
+++ main.go
@@ -0,0 +1,98 @@
+/**
+ *  Copyright 2024 dqx0
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/atotto/clipboard"
+	"github.com/getlantern/systray"
+	"github.com/pquerna/otp/totp"
+)
+
+func main() {
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	systray.SetIcon(icoToByteArray(os.Getenv("TOTP_ICON_PATH")))
+	systray.SetTitle("TOTP")
+	systray.SetTooltip("TOTP")
+
+	secret := os.Getenv("TOTP_KEY")
+
+	if secret == "" {
+		systray.AddMenuItem("No secret found", "No secret found")
+		return
+	}
+
+	copyMenuItem := systray.AddMenuItem("Copy", "Copy the TOTP code to the clipboard")
+	exitMenuItem := systray.AddMenuItem("Exit", "Exit the application")
+
+	go func() {
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+		for range ticker.C {
+			code, err := totp.GenerateCode(secret, time.Now())
+			if err != nil {
+				fmt.Println("Error generating TOTP code: ", err)
+				continue
+			}
+			systray.SetTooltip(code)
+		}
+	}()
+
+	go func() {
+		for range copyMenuItem.ClickedCh {
+			otp, err := totp.GenerateCode(secret, time.Now())
+			if err != nil {
+				fmt.Println("Error generating TOTP code: ", err)
+				continue
+			}
+			err = clipboard.WriteAll(otp)
+			if err != nil {
+				fmt.Println("Error copying TOTP code to clipboard: ", err)
+			} else {
+				fmt.Println("Copied TOTP code to clipboard")
+			}
+		}
+	}()
+	<-exitMenuItem.ClickedCh
+	systray.Quit()
+}
+
+func onExit() {
+}
+
+func icoToByteArray(path string) []byte {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil
+	}
+	defer file.Close()
+
+	byteArray, err := io.ReadAll(file)
+	if err != nil {
+		return nil
+	}
+
+	return byteArray
+}
